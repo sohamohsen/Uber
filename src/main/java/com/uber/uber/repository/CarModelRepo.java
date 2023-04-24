8b72d46b24cf688b2cffd8e78cb932841236c945
Ibrahim Magdy <ibrahim.magdy31@gmail.com>
2023-04-24 06:29:10 +0200

    get models by car id

--- src/main/java/com/uber/uber/repository/CarModelRepo.java
+++ src/main/java/com/uber/uber/repository/CarModelRepo.java
@@ -3,5 +3,10 @@ package com.uber.uber.repository;
 import com.uber.uber.models.CarModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CarModelRepo extends JpaRepository <CarModel, Integer> {
+
+    List<CarModel> findByCarMakerId(int carMakerId);
+
 }
